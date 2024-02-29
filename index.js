266ccefc037b1e9ca7a06226d3352727b195486f
Luiz Peralta <luizantonioperalta@gmail.com>
2024-02-29 17:56:23 -0300

    iniciando o projeto

---
+++ index.js
@@ -0,0 +1,6 @@
+function meuCodigo(){
+    console.log("Hello World!");
+}
+function outraFuncao() {
+    console.log("Finalizando a API de clientes");
+}
---
+++ outroArquivo.js
@@ -0,0 +1,1 @@
+console.log("Outro arquivo!");
